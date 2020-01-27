62a2f5a1484eee585c311192fa7c9decacc83888
sun ke <289034922@qq.com>
2020-01-27 03:41:59 -0500

    first commit

---
+++ src/main/java/edu/northeastern/cs5200/hello/HelloController.java
@@ -0,0 +1,18 @@
+package edu.northeastern.cs5200.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/api/hello/string")
+    public String sayHello() {
+        return "Hello Ke Sun!";
+    }
+	@RequestMapping("/api/hello/object")
+    public HelloObject sayHelloObject() {
+        HelloObject obj = new HelloObject("Hello Ke Sun!");
+        return obj;
+    }
+
+}
